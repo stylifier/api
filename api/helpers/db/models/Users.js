006a3efbbf663b030bf7542eb8b5be5777c03246
Ali <ali@Alis-iMac.fritz.box>
2018-10-18 12:45:53 +0200

    fix up login

--- api/helpers/db/models/Users.js
+++ api/helpers/db/models/Users.js
@@ -87,11 +87,13 @@ module.exports = (sequelize, Datatypes) => {
   }
 
   model.checkLogin = function(username, password) {
-    return this.findOne({where: {[Datatypes.Op.or]: [
-      {username: username.toLowerCase()},
-      {username: username.replace('m_g_i_o_s_', '').toLowerCase()},
-      {username: 'm_g_i_o_s_' + username.toLowerCase()}
-    ]}})
+    return this.findOne({where: {
+      username: {
+        [Datatypes.Op.or]: [
+          username.toLowerCase(),
+          username.replace('m_g_i_o_s_', '').toLowerCase()
+        ]}
+    }})
     .then(user => user.get('password'))
     .then(psw => bcrypt.compareSync(password, psw) ?
       Promise.resolve() :
